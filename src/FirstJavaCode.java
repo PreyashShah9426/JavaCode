3e3fc9be87126d18541a919c16291371f8e0391e
PreyashShah9426 <preyashshah9426@icloud.com>
2023-07-06 20:53:53 -0400

    Hello World

---
+++ src/FirstJavaCode.java
@@ -0,0 +1,23 @@
+public class FirstJavaCode {
+    public static void main (String[] args) {
+
+        int age = 22;
+        String name = "Preyash";
+        long population = 122122234234L;
+        double salary = 0.002201;
+        float bonus = 0.01f;
+        char note = 'p';
+        boolean not = true;
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(population);
+        System.out.println(salary);
+        System.out.println(bonus);
+        System.out.println(note);
+        System.out.println(not);
+
+
+
+    }
+}
